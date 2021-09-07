c703f8fc007cc09850aad72bc56706d44f51cb53
TipicultBiomassa <48147673+TipicultBiomassa@users.noreply.github.com>
2021-09-08 01:18:30 +0300

    1
    1

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      firstName: 'Пётр',
+      surName: 'Сидоров',
+    },
+    {
+      firstName: 'Любовь',
+      surName: 'Петровна',
+    },
+    {
+      firstName: 'Дмитрий',
+      surName: 'Вячеславович',
+    },
+   {
+      firstName: 'Ольга',
+      surName: 'Борисовна',
+    },
+    {
+      firstName: 'Василий',
+      surName: 'Ибрагимов',
+    },
+  ]
+
+  
